f7b4e4f8c7eca81ceef992b3da49636e36328469
pile23 <pile23@proton.me>
2024-10-08 11:30:02 +0200

    Update script.js

--- picerija1/script.js
+++ picerija1/script.js
@@ -1,16 +1,2 @@
 // script
 console.log("ucitan skript");
-
-// // let dropdownItems = dropdownItemDodaciZaPice.querySele
-// const dropdownItemDodaciZaPice = document.getElementById("dropdown-item-dodaci-za-pice");
-// dropdownItemDodaciZaPice.addEventListener("click", (event) => {
-//   console.log("event ", event.target);
-//   console.log("kliknuto na element");
-//   let karticaDodaciZaPicu = document.getElementById("dodaci-pice");
-//   // karticaDodaciZaPicu.className = karticaDodaciZaPicu.className.replace("border-0", " border-2 border-primary") ;
-//   karticaDodaciZaPicu.className += " border-primary";
-//   setTimeout(() => {
-//     karticaDodaciZaPicu.className = karticaDodaciZaPicu.className.replace("border-primary", "");
-//   }, 1800);
-// });
- 
